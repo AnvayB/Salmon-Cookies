69b26104a93b481b8a6e8be874a8d14b3598e57a
Anvay Bhanap <anvaybhanap@Anvays-MacBook-Pro.local>
2021-06-14 21:24:11 -0700

    update objects and sales.html

--- js/app.js
+++ js/app.js
@@ -1,25 +1,27 @@
 'use strict'
 
-//cookies purchased per hour = avgCookieSale * custPerHour
-
 const Seattle = {
-  // customers: 0,
-  munCust: 23,
+  minCust: 23,
   maxCust: 65,
   avgCookieSale: 6.3,
-  custPerHour: function () {
+  custPerHour: function() {
     return Math.float(Math.random)* 100;
   },
   cookiesPurchased: avgCookieSale * custPerHour(),
 
+  cookies: [],
+  cookieHr: function() {
+
+  },
+  
+
 }
 
 const Tokyo = {
-  customers: 0,
-  munCust: 3,
+  minCust: 3,
   maxCust: 24,
   avgCookieSale: 1.2,
-  custPerHour: function () {
+  custPerHour: function() {
     return Math.float(Math.random)* 100;
   },
   cookiesPurchased: avgCookieSale * custPerHour(),
@@ -27,22 +29,20 @@ const Tokyo = {
 }
 
 const Dubai = {
-  customers: 0,
-  munCust: 11,
+  minCust: 11,
   maxCust: 38,
   avgCookieSale: 3.7,
-  custPerHour: function () {
+  custPerHour: function() {
     return Math.float(Math.random)* 100;
   },
   cookiesPurchased: avgCookieSale * custPerHour(),
 }
 
 const Paris = {
-  customers: 0,
-  munCust: 20,
+  minCust: 20,
   maxCust: 38,
   avgCookieSale: 2.3,
-  custPerHour: function () {
+  custPerHour: function() {
     return Math.float(Math.random)* 100;
   },
   cookiesPurchased: avgCookieSale * custPerHour(),
@@ -50,11 +50,10 @@ const Paris = {
 }
 
 const Lima = {
-  customers: 0,
-  munCust: 2,
+  minCust: 2,
   maxCust: 16,
   avgCookieSale: 4.6,
-  custPerHour: function () {
+  custPerHour: function() {
     return Math.float(Math.random)* 100;
   },
   cookiesPurchased: avgCookieSale * custPerHour(),
