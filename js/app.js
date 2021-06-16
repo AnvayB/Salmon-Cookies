3333d1c91b3456537960b7a65f8f0bb0c025dcf2
Anvay Bhanap <anvaybhanap@Anvays-MacBook-Pro.local>
2021-06-15 22:47:59 -0700

    fixed css and table Total title

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var hours = [' ','6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
+var hours = [' ','6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Daily Location Total'];
 
 let locations = [];
 let referenceTable = document.getElementById('cookiestable');
